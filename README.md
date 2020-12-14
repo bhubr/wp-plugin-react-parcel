066b6116635be0847c3808ffe1e242283abd2cb7
Benoit Hubert <benoithubert@gmail.com>
2020-12-14 11:05:58 +0100

    Readme, comments, todo, start script

---
+++ README.md
@@ -0,0 +1,9 @@
+# Plugin WP React
+
+Test de plugin WordPress incluant une app React. Utilise Parcel pour le lancement.
+
+## Installation / utilisation
+
+* Installer les dépendances : `npm install`
+* Lancer : `npm start`
+
--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
 /**
- * Plugin Name: WeJob Public
+ * Plugin Name: WeJob Public Test
  * Version: 0.1
  * Author: Benoît Hubert
+ * TODO : gérer modes dev et prod
  */
 
- //[foobar]
 function wejob_public_app( $atts ){
   $body_inner = file_get_contents(__DIR__ . '/dist/entry.html');
   $plugin_base_url = plugins_url("wejob-public");
   $script_replaced = str_replace('src="/', "src=\"$plugin_base_url/dist/", $body_inner);
+  // Ajout d'un timestamp pour forcer le reload
   $script_replaced = str_replace('.js', ".js?ts=" . time(), $script_replaced);
   return $script_replaced;
 }
